30029aebe2b205842b78fc33ec463cf6818859cf
eldersol <eldertojins@gmail.com>
2020-04-05 14:46:52 -0600

    version 0.1 banco

---
+++ BANCO_JDEVELOPER/BANCO/ViewController/src/view/backing/INICIO.java
@@ -0,0 +1,64 @@
+package view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import oracle.adf.view.rich.component.rich.layout.RichPanelHeader;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+
+public class INICIO {
+    private RichForm f1;
+    private RichDocument d1;
+    private RichPanelHeader ph1;
+    private RichInputText it1;
+    private RichInputText it2;
+    private RichButton b1;
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setPh1(RichPanelHeader ph1) {
+        this.ph1 = ph1;
+    }
+
+    public RichPanelHeader getPh1() {
+        return ph1;
+    }
+
+    public void setIt1(RichInputText it1) {
+        this.it1 = it1;
+    }
+
+    public RichInputText getIt1() {
+        return it1;
+    }
+
+    public void setIt2(RichInputText it2) {
+        this.it2 = it2;
+    }
+
+    public RichInputText getIt2() {
+        return it2;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
+    }
+}
